2d16a24e3ebeca801ece854bf013a64b381f6514
Syrr Gordon <marcus.syrr@gmail.com>
2025-06-24 11:35:36 -0400

    created skeleton function for loop and added entry point in main.py

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 from dotenv import load_dotenv
 import os
+from loop import prompt_lock_loop
 
+# Import your API key from .env file
 load_dotenv()
 API_KEY = os.getenv("GROQ_API_KEY")
 print(f"Using API key: {API_KEY[:8]}")
+
+# Entry point
+if __name__ == "__main __":
+    prompt_lock_loop()
