50271434ad040ab1b26b90bd5094ff97a18a53ba
Syrr Gordon <marcus.syrr@gmail.com>
2025-06-24 13:02:22 -0400

    moved formatting code into function format_json()

--- formatter.py
+++ formatter.py
@@ -16,13 +16,14 @@ with open(file) as f:
 batch_size = 5
 
 # Creates batches of 5 questions each
-for i in range(0, len(data), batch_size):
-    batch = data[i:i+batch_size] # Holds 5 input-target pairs
-    
-    batch_inputs = ""
-    # Iterate through each key-value pair in batch
-    for idx, ex in enumerate(batch):
-        options = list(ex["target_scores"].keys()) # Isolate the 5 answer options
-        plain = "\n".join([f"- {opt}" for opt in options]) # Puts each option next to a dash for formatting
-        batch_inputs += f"{idx+1}. Input:\n{ex['input']}\nOptions:\n{plain}\n\n" # Formats answer and options into consistent schema
+def format_json():
+    for i in range(0, len(data), batch_size):
+        batch = data[i:i+batch_size] # Holds 5 input-target pairs
         
+        batch_inputs = ""
+        # Iterate through each key-value pair in batch
+        for idx, ex in enumerate(batch):
+            options = list(ex["target_scores"].keys()) # Isolate the 5 answer options
+            plain = "\n".join([f"- {opt}" for opt in options]) # Puts each option next to a dash for formatting
+            batch_inputs += f"{idx+1}. Input:\n{ex['input']}\nOptions:\n{plain}\n\n" # Formats answer and options into consistent schema
+
